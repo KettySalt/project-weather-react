260bcb12d863e9bcbf38cd33af367dc5031803e0
Ketty <yuriy.sev51@gmail.com>
2023-01-27 15:55:04 +0200

    Cleaned html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-// import logo from "./logo.svg";
 // import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return <Weather city="Kyiv" />;
 }
-
-export default App;
